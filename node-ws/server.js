ce2023e5da94d1f904883990496c2ad741f7c841
Livne Lang <livnelang@gmail.com>
2015-12-22 20:19:32 +0200

    22.12.15 Commit

--- node-ws/server.js
+++ node-ws/server.js
@@ -7,27 +7,40 @@ var decoder = Decoder64();
  / Main Test Rout
  */
 app.get('/', function(req,res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.set('Content-Type', 'text/html');
-    res.send('<html><body> <h1>Welcome, This is Base64 Decode Example </h1>'+
-        '<h2> Enjoy </h2></body></html>');
-});
-
-
-app.get('/hello', function(req,res) {
-    res.set('Content-Type', 'text/html');
-    res.send('<html><body> <h1>Hello Test </h1>'+
-        '<h2> Enjoy </h2></body></html>');
+    res.send('<html><body> <h1>Welcome, This is Heroku Test Server </h1>'+
+        '<h2> Route To hello/:name To Test your name with JSON Response </h2></body></html>');
 });
 
 /*
- / Decode Base64 Rout
+* Hello Route to test your Request
  */
-app.get('/decode64', function (req,res)  {
+app.get('/hello/:name', function(req,res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    var ua = req.headers['user-agent'];
+    var user_details = {
+        name: req.params.name,
+        browser: ua
+    }
+    app.set('json spaces', 4);
+    res.set("Content-Type", "application/json");
+    res.status(200);
+    res.json(user_details);
+});
+
+/*
+ / Decode Base64 Route
+ */
+app.get('/decode64/:base64', function (req,res)  {
     console.log('inside localhost:3000/decode64 \n');
     //console.log('req.params: ' + req.param('base64'));
     //console.log('base coming: ' +req.param('base64') + '\n');
 
-    var img = decoder.decode(req.param('base64'));
+    var img = decoder.decode(req.params.base64);
+    console.log('img link: ' + img);
     app.set('json space',4);
     res.set('Content-Type', 'text/html');
     res.send('<html><body> <h1>Welcome, you can choose 3 routing ways : </h1>'+
