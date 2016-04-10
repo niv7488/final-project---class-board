6e4004ca219c8d25e7d01560b52c36b92ee34771
lully1008 <laurachiche@me.com>
2016-04-10 22:38:21 +0300

    Add canvas

--- student/sketch/includes/script.js
+++ student/sketch/includes/script.js
@@ -49,6 +49,23 @@ class LessonContent {
     }
 }
 
+"undefined" === typeof boardCastObj && (boardCastObj = {});
+(function(boardCastObj) {
+    boardCastObj.test = boardCastObj.test || {};
+    var test = boardCastObj.test;
+    test.student = {
+        type: "Test",
+        name: "Test ForNow",
+        id: "1123",
+        coursesList: {
+            add: function(courseName, courseId) {
+                var newCourse = new Course(courseName, courseId);
+                test.coursesList[courseId] = newCourse;
+            }
+        }
+    };
+})(boardCastObj);
+
 "undefined" === typeof eventJs && (eventJs = {});
 "undefined" === typeof query && (query = {});
 //Initialize buttons
@@ -77,7 +94,7 @@ class LessonContent {
             $('#bc-iframe').attr('src', '');
         }
         else {
-            $('#bc-iframe').attr('src','http:\\\\192.168.2.103:7070');
+            $('#bc-iframe').attr('src','http:\\\\192.168.2.107:7070');
             $(".bc-boardcontent").show();
         }
     });
@@ -93,20 +110,14 @@ class LessonContent {
     });
 
     $('#board-tab').click(function() {
+        $('#gallery-content').empty();
         console.debug('[EventClick] board tab');
         query.createBoardList();
     });
 
     $('#notebook-tab').click(function() {
         console.debug('[EventClick] notebook tab');
-        var ulElmt = $('#gallery-content');
-        console.debug(ulElmt);
-        ulElmt.html("");
-        /*
-        $.each(ulElmt, function(key, value) {
-            $(key).remove();
-        })*/
-        //$('#gallery-content').remove("li");
+        $('#gallery-content').empty();
     });
 
     $(window).resize(function() {
@@ -195,20 +206,11 @@ class LessonContent {
             });
         });
         var createBoardList = function() {
-            console.log(student);
-            $.each(courses[99999].lessons[31032016].content, function(key, value) {
-                console.debug('[AddContentGallery] add content to gallery');
-                console.debug(value);
-                var li = $('<li/>')
-                    .addClass('board-content')
-                    .css("background-image", "url("+ value.src +")")
-                    .css("background-size","100% 140px");
-                li.appendTo('#gallery-content');
-            });
+            query.createBoardList();
         };
     };
     query.createBoardList = function() {
-        var courses = student.coursesList;
+        var courses = courses || student.coursesList || {};
         console.log(student);
         $.each(courses[99999].lessons[31032016].content, function(key, value) {
             console.debug('[AddContentGallery] add content to gallery');
@@ -239,4 +241,80 @@ class LessonContent {
 
     console.log(student);
     query.initializeApp();
+
+
+
 })(notebook);
+
+/*
+"undefined" === typeof boardCastObj && (boardCastObj || {});
+(function(boardCastObj) {
+    /*boardCastObj.canvas = {
+        paint: false,
+        addClick: {},
+        clickX: new Array(),
+        clickY: new Array(),
+        clickDrag: new Array(),
+        redraw: {},
+        context: 
+    }*//*
+    var bcCanvas = boardCastObj.canvas = boardCastObj.canvas || {};
+    var paint = bcCanvas.paint = bcCanvas.paint || false;
+    var addClick = bcCanvas.addClick = bcCanvas.addClick || {};
+    var clickX = bcCanvas.clickX = bcCanvas.clickX || new Array();
+    var clickY = bcCanvas.clickY = bcCanvas.clickY || new Array();
+    var clickDrag = bcCanvas.clickDrag = bcCanvas.clickDrag || new Array();
+    var redraw = bcCanvas.redraw = bcCanvas.redraw || {};
+    var context = boardCastObj.canvas.context = document.getElementById('canvas').getContext("2d");
+    boardCastObj.canvas.dom = $('#canvas');
+    var canvas = boardCastObj.canvas.dom;
+    console.debug($("#canvas"));
+    addClick = function(x, y, dragging) {
+        clickX.push(x);
+        clickY.push(y);
+        clickDrag.push(dragging);
+    };
+    $('#canvas').onmousedown(function(e) {
+        console.debug('Canvas mousedown');
+        var mouseX = e.pageX - this.offsetLeft;
+        var mouseY = e.pageY - this.offsetTop;     
+        paint = true;
+        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+        redraw();
+    });
+    $('#canvas').on("mousemove", function(e) {
+        console.debug('Canvas mousemouve');
+        if(paint) {
+            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+            redraw();
+        }
+    });
+    $('#canvas').on("mouseup", function(e) {
+        console.debug('Canvas mouseup');
+        paint = false;
+    });
+    $('#canvas').on("mouseleave", function(e) {
+        console.debug('Canvas mouseleave');
+        paint = false; 
+    });
+    redraw = function() {
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+  
+        context.strokeStyle = "#df4b26";
+        context.lineJoin = "round";
+        context.lineWidth = 5;
+                
+        for(var i=0; i < clickX.length; i++) {        
+            context.beginPath();
+            if(clickDrag[i] && i){
+                context.moveTo(clickX[i-1], clickY[i-1]);
+            }
+            else {
+                context.moveTo(clickX[i]-1, clickY[i]);
+            }
+            context.lineTo(clickX[i], clickY[i]);
+            context.closePath();
+            context.stroke();
+        }
+    };
+})(boardCastObj);*/
